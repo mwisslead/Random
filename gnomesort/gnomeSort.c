86b4fcb6aaf09a3e48b1720f3b460c74dbd111cd
Michael Wisslead <michael.wisslead@gmail.com>
2016-10-16 03:03:50 -0500

    Made gnome sort function more generic.

--- gnomesort/gnomeSort.c
+++ gnomesort/gnomeSort.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-void gnomeSort(char *data, int ldata)
+int comparisonFunction(void *data1, void *data2)
 {
-	int pos = 1;
-	while (pos < ldata){
-		if (data[pos] >= data[pos-1]){
-			pos++;
+	return *((char*) data1) >= *((char*) data2);
+}
+
+void swapMemory(void *mem1, void *mem2, int numBytes)
+{
+	char *data1 = mem1, *data2 = mem2;
+	for(int i = 0; i < numBytes; i++){
+		char t = data1[i];
+		data1[i] = data2[i];
+		data2[i] = t;
+	}
+}
+
+void gnomeSort(void *data, int size, int nmemb, int (comp)(void *, void*))
+{
+	int pos = size;
+	while (pos < nmemb*size){
+		if (comp(data+pos, data+pos-size)){
+			pos += size;
 		}else{
-			char t = data[pos];
-			data[pos] = data[pos-1];
-			data[pos-1] = t;
-			if (pos > 1){
-				pos--;
+			swapMemory(data+pos, data+pos-size, size);
+			if (pos > size){
+				pos -= size;
 			}
 		}
 	}
@@ -26,8 +39,16 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	for(i = 1; i < argc; i++){
-		fprintf(stdout, "%s : ", argv[i]);
-		gnomeSort(argv[i], strlen(argv[i]));
-		fprintf(stdout, "%s\n", argv[i]);
+		char *s = argv[i];
+		fprintf(stdout, "%s : ", s);
+		gnomeSort(s, sizeof(*s), strlen(s), comparisonFunction);
+		fprintf(stdout, "%s\n", s);
 	}
+	int x[5] = {1, 2, 255, 256, 257};
+	gnomeSort(x, sizeof(*x), sizeof(x)/sizeof(*x), comparisonFunction);
+	fprintf(stdout, "Sorted by first byte cast to char only: ");
+	for(i = 0; i < 5; i++){
+		fprintf(stdout, "%d, ", x[i]);
+	}
+	fprintf(stdout, "\n");
 }
