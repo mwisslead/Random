360bac03c798141d34d9c37d18622c8adabca393
Michael Wisslead <michael.wisslead@gmail.com>
2016-10-26 00:03:09 -0500

    Use whole words instead of sequence of characters.

--- markov_text_generator/test.py
+++ markov_text_generator/test.py
@@ -1,22 +1,23 @@
-from random import randint
+import random
 import re
 
 with open('moby_dick.txt') as fid:
    txt = fid.read()
 
-txt = txt.lower()
-txt = re.sub('[^a-z]', ' ', txt)
-txt = re.sub('\s+', ' ', txt)
+worder = re.compile('(\w+\'?\w+)').findall
+
+words = worder(txt.lower())
+words = [word.replace('\'s', '') for word in words]
 
 nlen = 6
 
 ngramdict = {}
 
-for i in range(nlen-1, len(txt)-1):
-    t = txt[i-nlen+1:i+1]
+for i in range(nlen-1, len(words)-1):
+    t = ' '.join(words[i-nlen+1:i+1])
     if t not in ngramdict:
         ngramdict[t] = {}
-    sym = txt[i+1]
+    sym = words[i+1]
     if sym not in ngramdict[t]:
         ngramdict[t][sym] = 1
     else:
@@ -29,15 +30,27 @@ for key in ngramdict:
         symdict[sym] += tot
         tot = symdict[sym]
 
-string = next(key for key in ngramdict if key.startswith('the'))
+string = worder(next(key for key in ngramdict))
+for i in range(100):
+    try:
+        symdict = ngramdict[' '.join(string[-nlen:])]
+        mx = max(symdict[key] for key in symdict)
+        val = random.randint(1, mx)
+        for key in symdict:
+            if val <= symdict[key] <= mx:
+                mx = symdict[key]
+                final_key = key
+        string += [final_key]
+    except:
+        string += worder(random.choice(list(ngramdict)))[0:1]
 
-for i in range(1000):
-    symdict = ngramdict[string[-nlen:]]
-    mx = max(symdict[key] for key in symdict)
-    val = randint(1, mx)
-    for key in symdict:
-       if val <= symdict[key]:
-           final_key = key
-    string += final_key
+output = ['']
+i = 0
+while string:
+    word = string.pop(0)
+    if len(output[i]) + len(word) > 100:
+        i += 1
+        output.append('')
+    output[i] += ' ' + word
 
-print(string)
+print('\n'.join([l.strip() for l in output]))
