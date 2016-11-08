430076f058828fdf8ca40605295d4a5887d04813
Michael Wisslead <michael.wisslead@gmail.com>
2016-11-08 02:06:57 -0600

    Added doc strings to thue morse code.

--- thue_morse/thue_morse.py
+++ thue_morse/thue_morse.py
@@ -1,10 +1,14 @@
+'''Generate sequence of n symbols that alternate in a fair manner'''
+
 def thue_morse(n, b=2):
+    '''generate the nth symbol given b symbols to choose from'''
     if n < b:
         return n
     r = n % b
     return (thue_morse((n-r)//b, b) + r) % b
 
 def main():
+    '''Main'''
     import matplotlib.pyplot as plt
     import numpy as np
     num = 3
